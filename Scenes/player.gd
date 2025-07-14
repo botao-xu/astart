0e36c1da646e39b224505abf2c28ae859cda5f9f
botao-xu <xbt1024@gmail.com>
2025-07-14 18:27:59 +0800

    Add boundaries

--- Scenes/player.gd
+++
@@ -1,8 +0,0 @@
-extends CharacterBody2D
-
-@export var move_speed :float = 50
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	velocity = Input.get_vector("left", "right", "up", "down") * move_speed
-	move_and_slide()
